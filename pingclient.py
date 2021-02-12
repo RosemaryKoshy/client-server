3fd6cbe51015a304b6ffa581e8ef30c771be9572
Talib Pierson <piersont@reed.edu>
2021-02-12 14:48:06 -0800

    statistic in spingclient.py

--- pingclient.py
+++ pingclient.py
@@ -8,17 +8,29 @@ import socket as s
 
 # SOCK_DGRAM for UDP
 SOCK = s.socket(s.AF_INET, s.SOCK_DGRAM)
+SOCK.settimeout(1)
 HOST = input('Enter host: ')
 PORT = int(input('Enter port: '))
 SOCK.connect((HOST, PORT))
 
-for i in range(10):
+MSG_COUNT = 10
+res_count = 0
+delay_tot = 0
+
+for i in range(MSG_COUNT):
     msg = f'ping {i}'
     t_0 = time.time()
     SOCK.send(msg.encode())
-    res = SOCK.recv(1024).decode()
-    t_1 = time.time()
-    if res != msg.upper():
-        print(f'{i} Error: received incorrect response')
-    else:
-        print(f'{i} RTT={(t_1 - t_0) * 1000} ms')
+    try:
+        res = SOCK.recv(1024).decode()
+        t_1 = time.time()
+        delay = (t_1 - t_0) * 1000
+        res_count += 1
+        delay_tot += delay
+        print(res)
+        print(f'{i} RTT={delay} ms')
+    except s.timeout as e:
+        print(f'{i} RTT=*')
+
+print('average delay:', delay_tot / MSG_COUNT)
+print('response rate:', res_count / MSG_COUNT)
