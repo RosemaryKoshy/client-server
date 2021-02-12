b68d97edf771b5474db44f284705e5fd236936f1
rosemary <rosemary@localhost.localdomain>
2021-02-11 22:00:14 -0800

    add files

---
+++ webclient.py
@@ -0,0 +1,14 @@
+# Import socket module
+from socket import *
+
+# Prepare server client
+# SOCK_STREAM for TCP, SOCK_DGRAM for UDP
+sock = socket(AF_INET, SOCK_STREAM)
+host = input('Server IP address: ')
+port = input('Server port: ')
+path = input('Object path: ')
+sock.connect((host, port))
+sock.send(f'GET http://{host}:{port}/{path} HTTP/1.1'.encode())
+msg = sock.recv(1024).decode()
+sock.close()
+print(msg)
---
+++ webserver.py
@@ -0,0 +1,43 @@
+# Import socket module
+from socket import *
+
+# Prepare server socket
+# SOCK_STREAM for TCP, SOCK_DGRAM for UDP
+serverSocket = socket(AF_INET, SOCK_STREAM)
+# Your code starts here
+serverSocket.bind(('', 42069))
+serverSocket.listen()
+# Your code ends here
+while True:
+    # Establish connection
+    print('Server listening...')
+    connectionSocket, addr = serverSocket.accept()  # Your code starts here # Your code ends here
+    try:
+        message = connectionSocket.recv(1024).decode()  # Your code starts here # Your code ends here
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()  # Your code starts here # Your code ends here
+
+        # Send one HTTP header line to socket
+        # Your code starts here
+        connectionSocket.send('HTTP/1.1 200 OK\r\n'.encode())
+        # Your code ends here
+
+        # Send object to client
+        for line in outputdata:
+            connectionSocket.send(line.encode())
+        connectionSocket.send('\r\n'.encode())
+
+        connectionSocket.close()
+    except IOError:
+        # Send 404 message
+        # Your code starts here
+        connectionSocket.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode())
+        # Your code ends here
+
+        # Close client socket
+        # Your code starts here
+        connectionSocket.close()
+        # Your code ends here
+
+serverSocket.close()
